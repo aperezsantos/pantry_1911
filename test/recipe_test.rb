76e5684324850fa167e64a797d6fffd9b0d95f50
Ana Perez Santos <a.jperez@yahoo.com>
2020-01-08 10:47:13 -0700

    Pantry It. 2: create test/method for adding ingredients to recipe

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -5,18 +5,26 @@ require './lib/ingredient'
 
 class RecipeTest < Minitest::Test
   def test_it_exists
-    ingredient1 = Ingredient.new("Cheese", "C", 100)
-    ingredient2 = Ingredient.new("Macaroni", "oz", 30)
     recipe = Recipe.new("Mac and Cheese")
 
     assert_instance_of Recipe, recipe
   end
 
   def test_it_has_attributes
+    recipe = Recipe.new("Mac and Cheese")
+
+    assert_equal "Mac and Cheese", recipe.name
+    assert_equal ({}), recipe.ingredients_required
+  end
+
+  def test_it_can_add_ingredients
     ingredient1 = Ingredient.new("Cheese", "C", 100)
     ingredient2 = Ingredient.new("Macaroni", "oz", 30)
     recipe = Recipe.new("Mac and Cheese")
 
-    assert_equal "Mac and Cheese", recipe.name
+    recipe.add_ingredient(ingredient1, 2)
+    recipe.add_ingredient(ingredient2, 8)
+    
+    assert_equal ({ingredient1 => 2, ingredient2 => 8}), recipe.ingredients_required
   end
 end
