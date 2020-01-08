d5146177b7a81d2d101281d48969bfba7f426660
Ana Perez Santos <a.jperez@yahoo.com>
2020-01-08 11:16:26 -0700

    Pantry It. 2: test/method that returns an array of ingredients

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -13,5 +13,10 @@ class Recipe
   def amount_required(ingredient)
     @ingredients_required[ingredient]
   end
-  
+
+  def ingredients
+    @ingredients_required.keys
+  end
+
+
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -28,7 +28,7 @@ class RecipeTest < Minitest::Test
     assert_equal ({ingredient1 => 2, ingredient2 => 8}), recipe.ingredients_required
   end
 
-  def test_it_can_return_amount_required_per_ingredent
+  def test_it_can_return_amount_required_per_ingredient
     ingredient1 = Ingredient.new("Cheese", "C", 100)
     ingredient2 = Ingredient.new("Macaroni", "oz", 30)
     recipe = Recipe.new("Mac and Cheese")
@@ -38,6 +38,16 @@ class RecipeTest < Minitest::Test
 
     assert_equal 2, recipe.amount_required(ingredient1)
     assert_equal 8, recipe.amount_required(ingredient2)
+  end
 
+  def test_it_can_return_ingredients
+    ingredient1 = Ingredient.new("Cheese", "C", 100)
+    ingredient2 = Ingredient.new("Macaroni", "oz", 30)
+    recipe = Recipe.new("Mac and Cheese")
+
+    recipe.add_ingredient(ingredient1, 2)
+    recipe.add_ingredient(ingredient2, 8)
+
+    assert_equal [ingredient1, ingredient2], recipe.ingredients
   end
 end
